d5de88715e78ae00591350f9ab40a22307c2ea28
r0nk <r0nk@simplecpu.com>
2022-03-18 14:39:09 -0500

    actually training on doom

--- gym/sample.py
+++ gym/sample.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 import gym
-from gym_doom.wrappers import ViZDoomEnv, ScreenWrapper
+import vizdoomgym
+import time
+import sys
 
 from stable_baselines3 import PPO
 
-#env = gym.make("CartPole-v1")
-#model = PPO("MlpPolicy", env, verbose=1)
-#model.learn(total_timesteps=10000)
-#obs = env.reset()
-#for i in range(1000):
-#    action, _states = model.predict(obs, deterministic=True)
-#    obs, reward, done, info = env.step(action)
-#    env.render()
-#    if done:
-#      obs = env.reset()
-#env.close()
+env = gym.make("VizdoomDefendCenter-v0")
 
-if __name__ == '__main__':
+if len(sys.argv) > 1:
+    model = PPO.load(sys.argv[1])
+else:
+    model = PPO("MlpPolicy", env, verbose=1,tensorboard_log="/tmp/sac/")
+    model.learn(total_timesteps=1e4)
 
-    viz_doom_renderer = False
-    dir_ = "E:\VizDOOM"
-    env = gym.make('Doom-ram-v0')
-    env = ViZDoomEnv(env, level='deadly_corridor', data_dir=dir_)
-    env = ScreenWrapper(env, dim=(100, 100),  render=True, dummy_obs=True)
+obs = env.reset()
 
-    env.playHuman()
+model.save("models/"+str(time.time())+"_saved_model.zip")
+for i in range(10000):
+    action, _states = model.predict(obs, deterministic=True)
+    time.sleep(1/60)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+      obs = env.reset()
+env.close()
+
